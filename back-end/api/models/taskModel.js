9b0bd515f055f20e09f4565e93b520cb4e37ceab
iago <iago.pferreiravr@gmail.com>
2021-11-03 19:26:44 -0300

    refactor(back-end): Refatorando o retorno do insertTaks
    Antes a insertTask retornava todos as tarefas, agora ela retorna somente a tarefa adicionada

--- back-end/api/models/taskModel.js
+++ back-end/api/models/taskModel.js
@@ -1,11 +1,10 @@
-const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const coll = 'tasks';
 
 const getAllTasks = async () => {
   const tasks = await connection()
-  .then((db) => db.collection(coll).find().toArray());
+    .then((db) => db.collection(coll).find().toArray());
 
   if (tasks.length === 0) return null;
 
@@ -15,16 +14,18 @@ const getAllTasks = async () => {
 const insertTask = async (name, status) => {
   const date = new Date().toLocaleDateString('br');
 
-  await connection()
+  const task = await connection()
     .then((db) => db.collection(coll).insertOne({
-      name, status, date
+      name, status, date,
     }))
     .then((result) => ({ ...result.ops[0] }));
 
-  const tasks = await getAllTasks();
-
-  return tasks;
+  return task;
 };
+
+// const updateTask = () => {
+
+// };
 
 module.exports = {
   getAllTasks,

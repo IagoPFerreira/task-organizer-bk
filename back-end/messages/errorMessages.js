f41979af336c793f7e74334bd4fd60dc2525aac1
iago <iago.pferreiravr@gmail.com>
2021-11-04 17:51:17 -0300

    refactor(back-end): Adicionando mais algumas frase de erro

--- back-end/messages/errorMessages.js
+++ back-end/messages/errorMessages.js
@@ -1,7 +1,10 @@
 module.exports = {
+  ALL_FILDES_FILLED: 'All fields must be filled.',
+  EMAIL_ALREADY_REGISTRED: 'Email already registered.',
+  INCORRECT_USERNAME_OR_PASSWORD: 'Incorrect username or password.',
   INVALID_ENTRIES: 'Invalid entries. Try again.',
-  EMAIL_ALREADY_REGISTRED: 'Email already registered',
-  ALL_FILDES_FILLED: 'All fields must be filled',
-  INCORRECT_USERNAME_OR_PASSWORD: 'Incorrect username or password',
-  NON_EXISTENTE_USER: 'Non-existent user',
+  NO_REGISTRED_TASKS: 'There are no registered tasks.',
+  NON_EXISTENTE_USER: 'Non-existent user.',
+  SERVER_ERROR: 'server error.',
+  TASK_NOT_FOUND: 'Task not found.',
 };

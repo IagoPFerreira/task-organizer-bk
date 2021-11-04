0073056204a84a6f4afcf07377f21138dccdbab4
iago <iago.pferreiravr@gmail.com>
2021-11-04 16:05:00 -0300

    refactor(back-end): Adicionando novas mensagens de erro

--- back-end/messages/errorMessages.js
+++ back-end/messages/errorMessages.js
@@ -1,4 +1,7 @@
 module.exports = {
   INVALID_ENTRIES: 'Invalid entries. Try again.',
   EMAIL_ALREADY_REGISTRED: 'Email already registered',
+  ALL_FILDES_FILLED: 'All fields must be filled',
+  INCORRECT_USERNAME_OR_PASSWORD: 'Incorrect username or password',
+  NON_EXISTENTE_USER: 'Non-existent user',
 };

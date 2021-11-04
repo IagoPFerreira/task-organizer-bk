f5462fcd225e11cff64a5f2392ff0ed342ea99af
iago <iago.pferreiravr@gmail.com>
2021-11-04 15:22:55 -0300

    refactor(back-end): Refatorando o a lógica de inserção de novos usuários, criando checagens no service antes de inserir

--- back-end/api/models/userModel.js
+++ back-end/api/models/userModel.js
@@ -11,12 +11,23 @@ const createUser = async (name, password, email) => {
       }))
     .then((result) => {
       const { password: _, ...userWithoutPassword } = result.ops[0];
-      return { user: userWithoutPassword };
+      const { _id } = userWithoutPassword;
+      return { name, email, id: _id };
     });
 
   return newUser;
 };
 
+const existingEmail = async (email) => {
+  const exists = await connection()
+    .then((db) => db.collection(coll).findOne({ email }));
+
+  if (exists) return false;
+
+  return true;
+};
+
 module.exports = {
   createUser,
+  existingEmail,
 };
--- back-end/api/services/userService.js
+++ back-end/api/services/userService.js
@@ -1,9 +1,30 @@
 const model = require('../models/userModel');
+const { EMAIL_ALREADY_REGISTRED, INVALID_ENTRIES } = require('../../messages/errorMessages');
+
+const checkInfo = (name, password, email) => {
+  const regex = /\S+@\S+\.\S+/;
+
+  if (!name || !password || !regex.test(email) || !email) {
+    return ({ status: 400, data: INVALID_ENTRIES });
+  }
+
+  return false;
+};
 
 const createUser = async ({ name, password, email }) => {
+  const infoCheck = checkInfo(name, password, email);
+
+  if (infoCheck) return infoCheck;
+
+  const existingEmail = await model.existingEmail(email);
+
+  if (!existingEmail) {
+    return ({ status: 409, data: EMAIL_ALREADY_REGISTRED });
+  }
+
   const newUser = await model.createUser(name, password, email);
 
-  return newUser;
+  return ({ status: 201, data: newUser });
 };
 
 module.exports = {

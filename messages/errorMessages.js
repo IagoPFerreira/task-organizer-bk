03fe2ec2d7a31b936f5088d134214dbec705d302
iago <iago.pferreiravr@gmail.com>
2021-11-05 13:15:42 -0300

    feat: Adicionando nova mensagem de erro

--- messages/errorMessages.js
+++ messages/errorMessages.js
@@ -4,6 +4,7 @@ module.exports = {
   INCORRECT_USERNAME_OR_PASSWORD: 'Incorrect username or password.',
   INVALID_ENTRIES: 'Invalid entries. Try again.',
   NO_REGISTRED_TASKS: 'There are no registered tasks.',
+  NO_REGISTRED_USERS: 'There are no registered users',
   NON_EXISTENTE_USER: 'Non-existent user.',
   SERVER_ERROR: 'server error.',
   TASK_NOT_FOUND: 'Task not found.',

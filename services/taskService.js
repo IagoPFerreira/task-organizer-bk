4a9a24d29f7c08a6129a86ee0137332fe3e3f387
iago <iago.pferreiravr@gmail.com>
2022-03-11 18:05:52 -0300

    refactor(tasks): taskService
    Removendo todos os 'status codes' das funcoes e deixando somente as mensagens quando necessarias

--- services/taskService.js
+++ services/taskService.js
@@ -1,56 +1,52 @@
 const model = require('../models/taskModel');
 const {
-  NO_REGISTRED_TASKS, TASK_NOT_FOUND, INVALID_ENTRIES, SERVER_ERROR,
+  TASK_NOT_FOUND, INVALID_ENTRIES,
 } = require('../messages/errorMessages');
 
 const getAllTasks = async ({ role, userId }) => {
   const tasks = role === 'admin' ? await model.getAllTasks() : await model.getTasksByUserId(userId);
 
-  if (!tasks) return ({ status: 404, data: NO_REGISTRED_TASKS });
+  if (!tasks) return false;
 
-  return ({ status: 200, data: tasks });
+  return tasks;
 };
 
 const getTaskById = async (id) => {
   const task = await model.getTaskById(id);
 
-  if (!task) return ({ status: 404, data: TASK_NOT_FOUND });
+  if (!task) return false;
 
-  return ({ status: 200, data: task });
+  return (task);
 };
 
 const insertTask = async ({ name, status }, { userId }) => {
-  if (!name || !status) return ({ status: 400, data: INVALID_ENTRIES });
+  if (!name || !status) return ({ message: INVALID_ENTRIES });
 
   const task = await model.insertTask(name, status, userId);
 
-  if (!task) return ({ status: 500, data: SERVER_ERROR });
+  if (!task) return false;
 
-  return ({ status: 201, data: task });
+  return (task);
 };
 
 const updateTask = async (id, { name, status, date }) => {
-  if (!name || !status || !id) {
-    return (
-      { status: 400, data: INVALID_ENTRIES }
-    );
-  }
+  if (!name || !status || !id) return ({ message: INVALID_ENTRIES });
 
   const task = await model.updateTask(id, { name, status, date });
 
   if (!task) return ({ status: 404, data: TASK_NOT_FOUND });
 
-  return ({ status: 200, data: task });
+  return (task);
 };
 
 const deleteTask = async (id) => {
-  if (!id) return ({ status: 400, data: INVALID_ENTRIES });
+  if (!id) return ({ message: INVALID_ENTRIES });
 
   const task = await model.deleteTask(id);
 
-  if (!task) return ({ status: 404, data: TASK_NOT_FOUND });
+  if (!task) return false;
 
-  return ({ status: 204 });
+  return true;
 };
 
 module.exports = {

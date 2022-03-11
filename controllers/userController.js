68d8a61d250edce229f8a14f1ebe32c101b6886d
iago <iago.pferreiravr@gmail.com>
2022-03-11 20:05:57 -0300

    refactor(users): userController
    Alterando todas as funcoes para utilizarem o middleware de erro
    
    Trazendo para a camada de 'Controller' todos os 'status codes', deixando a responsabilidade de toda e qualquer interacao web na camada de 'Controller'

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,50 @@
+const { INVALID_ENTRIES, ONLY_ADMINS_REGISTER } = require('../messages/errorMessages');
 const service = require('../services/userService');
 
-const createUser = async (req, res) => {
+const createUser = async (req, res, next) => {
   const { body } = req;
 
-  const { status, data } = await service.createUser(body);
+  const user = await service.createUser(body);
 
-  return res.status(status).json({ data });
-};
-
-const login = async (req, res) => {
-  const { body } = req;
-
-  const { status, data, token } = await service.login(body);
-
-  if (token) {
-    return res.status(status).json({ token });
+  if (user.message) {
+    const code = user.message === INVALID_ENTRIES ? 400 : 409;
+    return next({ message: user.message, code });
   }
 
-  return res.status(status).json({ data });
+  return res.status(201).json({ data: user });
 };
 
-const findAllUsers = async (_req, res) => {
-  const { status, data } = await service.findAllUsers();
+const login = async (req, res, next) => {
+  const { body } = req;
 
-  return res.status(status).json({ data });
+  const token = await service.login(body);
+
+  if (token.message) {
+    return next({ message: token.message, code: 401 });
+  }
+
+  return res.status(200).json({ token });
 };
 
-const createAdmin = async (req, res) => {
+const findAllUsers = async (_req, res, next) => {
+  const users = await service.findAllUsers();
+
+  if (users.message) return next({ message: users.message, code: 404 });
+
+  return res.status(200).json({ data: users });
+};
+
+const createAdmin = async (req, res, next) => {
   const { body, user } = req;
 
-  const { status, data } = await service.createAdmin(body, user);
+  const newAdmin = await service.createAdmin(body, user);
 
-  return res.status(status).json({ data });
+  if (newAdmin.message) {
+    const code = user.message === ONLY_ADMINS_REGISTER ? 404 : 501;
+    return next({ message: newAdmin.message, code });
+  }
+
+  return res.status(201).json({ data: newAdmin });
 };
 
 module.exports = {

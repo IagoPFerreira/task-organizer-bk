70cf044178fd4b7356f03dda21a1b4eedcef7366
iago <iago.pferreiravr@gmail.com>
2022-03-11 20:05:10 -0300

    refactor(users): userService
    Removendo todos os 'status codes' das funcoes e deixando somente as mensagens quando necessarias

--- services/userService.js
+++ services/userService.js
@@ -19,7 +19,7 @@ const checkInfo = (name, password, email) => {
   const regex = /\S+@\S+\.\S+/;
 
   if (!name || !password || !regex.test(email) || !email) {
-    return ({ status: 400, data: INVALID_ENTRIES });
+    return ({ message: INVALID_ENTRIES });
   }
 
   return false;
@@ -33,38 +33,38 @@ const createUser = async ({ name, password, email }) => {
   const existingEmail = await model.existingEmail(email);
 
   if (existingEmail !== null) {
-    return ({ status: 409, data: EMAIL_ALREADY_REGISTRED });
+    return ({ message: EMAIL_ALREADY_REGISTRED });
   }
 
   const newUser = await model.createUser(name, password, email);
 
-  return ({ status: 201, data: newUser });
+  return (newUser);
 };
 
 const login = async ({ email, password }) => {
   if (!email || !password) {
-    return ({ status: 401, data: ALL_FILDES_FILLED });
+    return ({ message: ALL_FILDES_FILLED });
   }
 
   const loginCheck = await model.checkLogin(email, password);
 
   if (!loginCheck) {
-    return ({ status: 401, data: INCORRECT_USERNAME_OR_PASSWORD });
+    return ({ message: INCORRECT_USERNAME_OR_PASSWORD });
   }
 
   const token = jwt.sign(loginCheck, secret, jwtConfig);
 
-  return ({ status: 200, token });
+  return (token);
 };
 
 const findAllUsers = async () => {
   const allUsers = await model.findAllUsers();
 
   if (allUsers.length === 0) {
-    return ({ status: 404, data: NO_REGISTRED_USERS });
+    return ({ message: NO_REGISTRED_USERS });
   }
 
-  return ({ status: 200, data: allUsers });
+  return (allUsers);
 };
 
 const createAdmin = async ({
@@ -73,14 +73,14 @@ const createAdmin = async ({
   const { role: adminRole } = await model.existingEmail(user.email);
 
   if (adminRole !== 'admin') {
-    return ({ status: 403, data: ONLY_ADMINS_REGISTER });
+    return ({ message: ONLY_ADMINS_REGISTER });
   }
 
   const newAdmin = await model.createAdmin(name, password, email, role);
 
-  if (!newAdmin) return ({ status: 501, data: SERVER_ERROR });
+  if (!newAdmin) return ({ message: SERVER_ERROR });
 
-  return ({ status: 201, data: newAdmin });
+  return (newAdmin);
 };
 
 module.exports = {
